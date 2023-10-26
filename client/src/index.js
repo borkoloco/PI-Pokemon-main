4b3cb60baa75097114a37ae287c560b9fc84d851
Santiago Bosca Chillida <borkoloco@MacBook-Air-de-Santiago.local>
2023-10-26 03:55:56 -0300

    deploy

--- client/src/index.js
+++ client/src/index.js
@@ -8,8 +8,8 @@ import { Provider } from "react-redux";
 import store from "./redux/store";
 import axios from "axios";
 
-// axios.defaults.baseURL = render url;
-axios.defaults.baseURL = "http://localhost:3001";
+axios.defaults.baseURL = "https://pokemon-api-7qvi.onrender.com";
+// axios.defaults.baseURL = "http://localhost:3001";
 
 ReactDOM.render(
   <Provider store={store}>
